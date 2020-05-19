8f895347572d84039347319a4efa83ccd8ae7dfd
Rui Ma <mrsckd3035@gmail.com>
2020-05-19 16:19:53 -0500

    Create a function to get current weather using the current location

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const myWeatherAPIKey = '04901bda849c03c85e34cc2001a5c026';
 
-
 navigator.geolocation.getCurrentPosition(position => {
   localLat = position.coords.latitude;
   localLong = position.coords.longitude;
 
-  console.log(localLat);
-  console.log(localLong);
-})
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${localLat}&lon=${localLong}&appid=${myWeatherAPIKey}`)
+  .then(resp => {
+    if(resp.ok) {
+      return resp.json();
+    } else {
+      throw new Error('No json found');
+    }
+  })
+  .then(localWeather => {
+    console.log(localWeather);
+  })
+});
