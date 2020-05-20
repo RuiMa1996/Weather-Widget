c8cbf296f600128c194c2d6fc20321341f272fac
Rui Ma <mrsckd3035@gmail.com>
2020-05-20 10:23:52 -0500

    Find out the highest and lowest temp each day

--- src/app.js
+++ src/app.js
@@ -8,29 +8,29 @@ navigator.geolocation.getCurrentPosition(position => {
   localLong = position.coords.longitude;
 
   fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${localLat}&lon=${localLong}&appid=${myWeatherAPIKey}`)
-  .then(resp => {
-    if(resp.ok) {
-      return resp.json();
-    } else {
-      throw new Error('No json found');
-    }
-  })
-  .then(localWeather => {
-    displayLocalWeather(localWeather);
-  });
+    .then(resp => {
+      if (resp.ok) {
+        return resp.json();
+      } else {
+        throw new Error('No json found');
+      }
+    })
+    .then(localWeather => {
+      displayLocalWeather(localWeather);
+    });
 
   fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${localLat}&lon=${localLong}&appid=${myWeatherAPIKey}`)
-  .then(resp => {
-    if(resp.ok) {
-      return resp.json();
-    } else {
-      throw new Error('No json found');
-    }
-  })
-  .then(forecast => {
-    console.log(forecast);
-    displayForecast(forecast);
-  })
+    .then(resp => {
+      if (resp.ok) {
+        return resp.json();
+      } else {
+        throw new Error('No json found');
+      }
+    })
+    .then(forecast => {
+      console.log(forecast);
+      displayForecast(forecast);
+    })
 });
 
 function displayLocalWeather(info) {
@@ -46,32 +46,43 @@ function displayLocalWeather(info) {
   `)
 }
 
-function tempConvert (kelvinTemp) {
+function tempConvert(kelvinTemp) {
   return Math.round(kelvinTemp - 273.15);
 }
 
 function displayForecast(forecastInfo) {
   newDateObj = {};
 
-  for(let objOfArray of forecastInfo.list) {
-      if(objOfArray.dt_txt.includes(objOfArray.dt_txt.substring(0, 11))) {
-        if(newDateObj[convertTime(objOfArray.dt_txt)] === undefined) {
-          newDateObj[convertTime(objOfArray.dt_txt)] = [objOfArray];
-        } else {
-          newDateObj[convertTime(objOfArray.dt_txt)].push(objOfArray);
-        }
+  for (let objOfArray of forecastInfo.list) {
+    if (objOfArray.dt_txt.includes(objOfArray.dt_txt.substring(0, 11))) {
+      if (newDateObj[convertTime(objOfArray.dt_txt)] === undefined) {
+        newDateObj[convertTime(objOfArray.dt_txt)] = [objOfArray];
+      } else {
+        newDateObj[convertTime(objOfArray.dt_txt)].push(objOfArray);
       }
     }
-
-
+  }
   console.log(newDateObj);
-  return newDateObj;
-}
 
+  for (let weekdays in newDateObj) {
+    let date = new Date();
+    let highTemp = [];
+    let lowTemp = [];
+    if (weekdays !== dateOfWeek[date.getDay()]) {
+      for (let results of newDateObj[weekdays]) {
+        highTemp.push(results.main.temp_max);
+        highTemp.sort((a, b) => b - a);
+        lowTemp.push(results.main.temp_min);
+        lowTemp.sort((a, b) => a - b);
+      }
+      console.log(highTemp);
+      console.log(lowTemp);
+    }
+  }
+}
 
 
 function convertTime(time) {
   let newDate = new Date(time);
   return dateOfWeek[newDate.getDay()];
 }
-
